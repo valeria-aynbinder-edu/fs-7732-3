4f58682b0f98ac5b44ad1f53de32870aa7c305db
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-10 14:26:06 +0200

    lesson 3 template

---
+++ lesson3/lesson_code.py
@@ -0,0 +1,30 @@
+# string slicing - refreshment
+
+# more built-in functions
+# functions we've seen already: print() input()
+# casting: str() int() bool()
+# more: len() round()
+
+# string methods - creating a new string
+# operators + - <=
+# built-in functions: print(), input(), round(), int()
+# methods: text.upper()
+
+
+# string methods - split
+
+
+# more operators: in, not
+
+
+# lists
+# slicing operator
+# nested lists
+
+# lists slicing exercise
+
+
+# airplane seats questions
+
+
+# list methods
