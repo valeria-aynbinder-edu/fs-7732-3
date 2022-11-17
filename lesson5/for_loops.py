c70afda027b42998724694eb88fb346344adf2c5
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-17 18:13:07 +0200

    lesson 5

---
+++ lesson5/for_loops.py
@@ -0,0 +1,30 @@
+# i=0
+# while i < 10:
+#     print("********")
+#     i += 1
+#
+# grades = [99, 98, 97, 80, 100, 75, 93]
+# i = 0
+# while i < len(grades):
+#     print(grades[i])
+
+
+
+# for iterating list
+# for grade in grades:
+#     print(grade)
+
+
+# grades = [99, 98, 97, 80, 100, 75, 93]
+# names = ['Herut', 'Lior', 'Chen', 'Gal']
+# grades = []
+# for name in names:
+#     grade = int(input(f"Insert a grade for {name}"))
+#     grades.append(grade)
+# for i, name in enumerate(names):
+#     print(f"Record number {i}: The grade of {name} is {grades[i]}")
+
+
+# word = "Thursday"
+# for char in word:
+#     print(char)
