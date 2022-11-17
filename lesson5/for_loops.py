dfa31a3d1400b20e1a69aac360072d0fdb44e772
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-17 19:19:31 +0200

    lesson 5 upd

--- lesson5/for_loops.py
+++ lesson5/for_loops.py
@@ -28,3 +28,34 @@
 # word = "Thursday"
 # for char in word:
 #     print(char)
+
+
+# r = range(1_000_000)
+# print(50_098 in r)
+# print(r[5:15])
+# for num in range(10):
+#     print(num)
+
+# for num in range(-5, 20, 3):
+#     print(num)
+
+# print(range(-5, 20, 3))
+
+# my_range = range(-5, 0)
+# print(my_range)
+
+# cities = ['New York', 'Kyiv', 'Paris', 'London', 'Tel Aviv']
+# countries = ['USA', 'Ukraine', 'France', 'UK', 'Israel']
+#
+# for i in range(len(cities)-1, -1, -1):
+#     print(f"{countries[i]} - {cities[i]}")
+
+
+# find max
+nums = [54, -1, 45, 987, 5, 2, 65, 7, 12]
+max_num = nums[0]
+
+for num in nums:
+    if num > max_num:
+        max_num = num
+print(max_num)
