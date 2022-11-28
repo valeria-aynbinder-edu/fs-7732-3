a668fad4bebfaa18b008be10909d3b60ef43055a
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-11-28 20:22:45 +0200

    lesson 7 upd stocks

---
+++ lesson7/stocks_solution.py
@@ -0,0 +1,41 @@
+stocks_data = {
+    'TSLA': {
+        'ticker': 'TSLA',
+        'company_name': 'Tesla',
+        'employees_number': 5000,
+        'address': 'Claifornia',
+        'CEO_name': 'Elon Musk',
+        'stock_data':{
+            '14.11.2021': {
+                'open': 1001.5,
+                'close': 1020,
+                'volume': 50000000
+            },
+            '15.11.2021':{
+                'open': 1067.7,
+                'close': 1045.5,
+                'volume': 45000345
+            }
+        },
+    },
+
+    'AAPL': {
+        'ticker': 'AAPL',
+        'company_name': 'Apple',
+        'employees_number': 10000,
+        'address': 'New York',
+        'CEO_name': 'Tim Cook',
+        'stock_data': {
+            '14.11.2021': {
+                'open': 320.7,
+                'close': 322.8,
+                'volume': 5_345_333
+            },
+            '15.11.2021': {
+                'open': 300.7,
+                'close': 320.5,
+                'volume': 4_000_345
+            }
+        }
+    }
+}
