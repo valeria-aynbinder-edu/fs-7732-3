b55135038bd5471ee57b543291dd7a34e4120259
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-12-12 18:59:42 +0200

    lesson 10

---
+++ lesson10/car.py
@@ -0,0 +1,83 @@
+# my_car = {
+#     'manufacturer': 'Mazda',
+#     'model': '3',
+#     'color': 'white',
+#     'license_plate': '1234567',
+#     'year': 2015,
+#     'fuel': 50,
+#     'km': 102000,
+#     'fuel_consumption': 20
+# }
+#
+# def add_fuel(car: dict, liters):
+#     car['fuel'] += liters
+#
+# def drive(car, km):
+#     car['km'] += km
+    # update fuel
+#
+
+# car = {}
+# car['manufacturer'] = 'Mazda'
+
+
+class Car:
+    def __init__(self, man: str, mod: str, col, year,
+                 fuel_consumption, tank_capacity):
+        self.manufacturer = man
+        self.model = mod
+        self.color = col
+        self.year = year
+        # liters per 100 km
+        self.fuel_consumption = fuel_consumption
+        self.tank_capacity = tank_capacity
+
+        self.km = 0
+        self.fuel = 0
+
+    def fill_tank(self, liters) -> bool:
+        if 0 < liters <= self.tank_capacity - self.fuel:
+            self.fuel += liters
+            return True
+        return False
+
+
+
+car_mazda = Car('Mazda', '3', 'white', 2015, 20, 50)
+# car_mazda = Car.__init__(None, 'Mazda', '3',)
+car_toyota = Car('Toyota', 'Yaris', 'red', 2022, 10, 35)
+
+ret_val = car_mazda.fill_tank(30)
+print(f"retval is: {ret_val}, new fuel is mazda: {car_mazda.fuel}")
+print(f"new fuel is toyota: {car_toyota.fuel}")
+ret_val = car_mazda.fill_tank(40)
+print(f"retval is: {ret_val}, new fuel is mazda: {car_mazda.fuel}")
+
+print(isinstance(car_mazda, Car))
+print(car_mazda)
+
+
+# Car.fill_tank(car_mazda, 10)
+
+# fill tank
+# car_mazda.fuel += 50
+
+# l1 = list([3,4,5])
+# print(isinstance(l1, Car))
+# l2 = list([5,6,78])
+#
+# print("type of l1:", type(l1))
+# print("type of mazda_car: ", type(car_mazda))
+
+# car_mazda.color = 'yellow'
+# print(car_mazda.color)
+
+
+# car = {}
+
+# car1 = Car()
+# car1 = Car("Mazda", "3", "white")
+# print(type(car1))
+# s1 = set([2,3,4,4])
+# l1 = list()
+# str1 = str()
