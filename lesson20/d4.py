5e1fd068fd829d58c7509f8405de29f66e38f79d
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-01-23 21:48:21 +0200

    jb - less 20

--- lesson20/d4.py
+++ lesson20/d4.py
@@ -22,5 +22,13 @@ def filter_dates(dates_list):
     filtered_dates = filter(lambda date_obj: date_obj.strftime("%a") not in ("fri", "sat"), dates_mapping)
     return list(filtered_dates)
 
+
+# def sort_strings_by_len(strs_list: list[str]) -> list[str]:
+#     return list(sorted(strs_list, key=len))
+#
+#
+# if __name__ == '__main__':
+#     print(sort_strings_by_len(['clouds', 'sun', 'weather', 'rain', 'snow', 'heat', 'freeze']))
+
 if __name__ == '__main__':
     print(filter_dates(['12-12-2021', '18-12-2021', '19-12-2021']))
