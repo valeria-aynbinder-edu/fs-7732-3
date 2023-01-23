962ba91007aebf605442e8021264353bb995fb42
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-01-23 20:37:28 +0200

    jb - less 20

--- lesson20/d4.py
+++ lesson20/d4.py
@@ -1,7 +1,26 @@
-# d4 - ex 2
-def filter_out_vowels(word: str) -> str:
-    return "".join(filter(lambda c: c not in "aAeEiIoOuU", word))
+# # d4 - ex 2
+# def filter_out_vowels(word: str) -> str:
+#     return "".join(filter(lambda c: c not in "aAeEiIoOuU", word))
+#
+#
+# if __name__ == '__main__':
+#     print(filter_out_vowels('The Sun is shining'))
 
+
+# d4 - ex 3
+from datetime import datetime
+
+
+def filter_dates(dates_list):
+
+    dates_mapping = map(lambda date_str: datetime.strptime(date_str, "%d-%m-%Y"), dates_list)
+
+    #option 1
+    # filtered_dates = filter(lambda date_obj: date_obj.weekday() not in (4, 5), dates_mapping)
+
+    # option 2
+    filtered_dates = filter(lambda date_obj: date_obj.strftime("%a") not in ("fri", "sat"), dates_mapping)
+    return list(filtered_dates)
 
 if __name__ == '__main__':
-    print(filter_out_vowels('The Sun is shining'))
+    print(filter_dates(['12-12-2021', '18-12-2021', '19-12-2021']))
