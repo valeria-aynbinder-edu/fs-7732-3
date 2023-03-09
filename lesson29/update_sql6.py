877deaa469bf7948858d483bc23e85b1a0f72042
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-03-09 20:28:49 +0200

    jb - less 29

--- lesson29/update_sql6.py
+++ lesson29/update_sql6.py
@@ -36,6 +36,35 @@ def insert_customer(passport_num, name, address):
     return updated_rows
 
 
+def insert_customer_with_context_manager(passport_num, name, address):
+    """ update vendor name based on the vendor id """
+    sql = """ INSERT INTO customers
+                (passport_num, name, address)
+                VALUES
+                (%s, %s, %s)"""
+    conn = None
+    updated_rows = 0
+    try:
+        with psycopg2.connect(host="localhost",
+                port=5432,
+                database="bank",
+                user="postgres",
+                password="postgres") as conn:
+            # create a new cursor
+            with conn.cursor() as cur:
+                # execute the UPDATE  statement
+                cur.execute(sql, (passport_num, name, address))
+                # get the number of updated rows
+                updated_rows = cur.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return updated_rows
+
+
 if __name__ == '__main__':
     # Insert customer
     insert_customer(12121212, 'Stieven Spielberg', 'California')
