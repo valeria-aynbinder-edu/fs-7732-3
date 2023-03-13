dfeb7a8c13ed844b3c3d70a660ad925b0ef12848
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-03-13 10:58:48 +0200

    lesson30

--- lesson30/flask_base.py
+++ lesson30/flask_base.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask("sample_web_app")
 
@@ -22,8 +22,6 @@ def beauty():
            "</div>"
 
 
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555)
+    app.run(host='0.0.0.0', port=5554, debug=True)
     # app.run(debug=True, port=5001)
